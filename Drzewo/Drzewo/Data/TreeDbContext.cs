34cbbac8ec41cb37d6867ec8de051de4f27f8322
Maciek Pietrasz <167832@stud.prz.edu.pl>
2023-05-31 21:33:54 +0200

    DbContext, Models

---
+++ Drzewo/Drzewo/Data/TreeDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tree.Models;
+
+namespace Tree.Data
+{
+    public class TreeDbContext : DbContext
+    {
+        public TreeDbContext(DbContextOptions<TreeDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Node> Nodes { get; set; }
+
+
+        // When a Node (parent) is deleted, all its associated leaves (children) will also be automatically deleted.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Node>()
+                .HasMany(n => n.Children)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("TreeDb");
+            }
+        }
+    }
+}
---
+++ Drzewo/Drzewo/Models/Node.cs
@@ -0,0 +1,9 @@
+﻿namespace Tree.Models
+{
+    public class Node // element in the tree
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Node> Children { get; set; } = new List<Node>();
+    }
+}
